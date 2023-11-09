5e9d05066daf52ce77373aa86d6f2fab12e2f9ca
Marcocholla01 <marcochollapaul01@gmail.com>
2023-11-10 01:57:01 +0300

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB).then(()=>{
+    console.log("DB Connnection Established Sucessfully")
+}).catch((err)=>{
+    console.log(err.message)
+})
 
 const port = 3000 || process.env.PORT;
 const app =  express();
