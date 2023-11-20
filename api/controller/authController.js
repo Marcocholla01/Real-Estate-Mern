1184c5b83296e603844f3ff933654942caf1bc62
Marcocholla01 <marcochollapaul01@gmail.com>
2023-11-21 00:17:25 +0300

    Adding Bcryptjs And Cors packages

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,15 +1,15 @@
 import User from "../models/userModels.js";
 import { errorHandler } from "../utils/error.js";
-// import bcryptjs from "bcryptjs";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  // const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: password });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json("User Created Successfully");
   } catch (error) {
-    next(errorHandler(550, "Duplicat"));
+    next(errorHandler(550, "Duplicate"));
   }
 };
--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors"
 import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
+
 
 dotenv.config();
 
@@ -19,6 +21,7 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
@@ -30,7 +33,7 @@ app.use(`/api/auth`, authRouter);
 // Error Handler Middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internam server error";
+  const message = err.message || "Internal server error";
 
   return res.status(statusCode).json({
     success: false,
